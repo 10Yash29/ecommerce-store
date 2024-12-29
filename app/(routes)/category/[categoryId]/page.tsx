938e6c9fcf6f76fd0a8ae0a20123f1731cb319a7
Yash Kumar <yash29102004.com>
2024-12-30 02:23:48 +0530

    commit

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -4,54 +4,60 @@ import getColors from "@/actions/get-colors";
 import getCategory from "@/actions/get-category";
 import Container from "@/components/ui/container";
 import Billboard from "@/components/Billboard";
-import Filter from "@/app/(routes)/category/[categoryId]/components/filter";
+import Filter from "./components/filter";
 import ProductCard from "@/components/ProductCard";
-import {Products} from "@/types";
+import { Products } from "@/types";
 
-export const revalidate=0;
+// Ensure revalidation
+export const revalidate = 0;
 
-interface CategoryPageProps{
-    params:{
-        categoryId: string
-    }
-    searchParams:{
-        colorId:string
-        sizeId: string
-    }
+interface CategoryPageProps {
+    params: {
+        categoryId: string;
+    };
+    searchParams: {
+        colorId?: string; // Use optional chaining if these are not always present
+        sizeId?: string;
+    };
 }
 
-const CategoryPage = async ({params, searchParams}:CategoryPageProps)=>{
+const CategoryPage = async ({
+                                params,
+                                searchParams,
+                            }: CategoryPageProps) => {
+    // Fetch data
     const products = await getProducts({
         categoryId: params.categoryId,
-        colorId: searchParams.colorId,
-        sizeId: searchParams.sizeId,
-        }
-    );
-    const size = await getSizes();
+        colorId: searchParams.colorId || undefined,
+        sizeId: searchParams.sizeId || undefined,
+    });
+
+    const sizes = await getSizes();
     const colors = await getColors();
-    const category = await getCategory(params.categoryId)
+    const category = await getCategory(params.categoryId);
+
     return (
         <div className="bg-white">
             <Container>
-                <Billboard data={category.billboard}/>
+                <Billboard data={category.billboard} />
                 <div className="px-4 sm:px-6 lg:px-8 pb-24">
                     <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
                         <div className="hidden lg:block">
-                            <Filter data={size} valueKey='sizeId' name='Sizes'/>
-                            <Filter data={colors} valueKey='colorId' name='Colors'/>
+                            <Filter data={sizes} valueKey="sizeId" name="Sizes" />
+                            <Filter data={colors} valueKey="colorId" name="Colors" />
                         </div>
                         <div className="mt-6 lg:col-span-4 lg:mt-0">
                             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
                                 {products?.map((product: Products) => (
-                                    <ProductCard data={product} key={product.id}/>
+                                    <ProductCard data={product} key={product.id} />
                                 ))}
                             </div>
                         </div>
                     </div>
                 </div>
-
             </Container>
         </div>
     );
 };
+
 export default CategoryPage;
