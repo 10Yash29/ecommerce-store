4988f2fe832ec413b3fb2d8d3ed4f11b96bc30ee
Yash Kumar <yash29102004.com>
2025-01-14 16:03:45 +0530

    Merge remote-tracking branch 'origin/main'

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,19 +3,19 @@ import Link from "next/link";
 import MainNav from "@/components/MainNav";
 import getCategories from "@/actions/get-categories";
 import NavBarActions from "@/components/NavBarActions";
+import Image from "next/image";
 
 const Navbar = async () => {
-    const categories = await getCategories(); // Fetch categories dynamically
+    const categories = await getCategories();
 
     return (
         <div className="sticky top-0 z-50 bg-white shadow-md">
             <Container>
                 <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center justify-between">
-                    {/* Store Logo */}
                     <Link href="/" className="flex items-center gap-x-3 group">
                         <div className="relative">
                             <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 p-[2px] rounded-full shadow-md group-hover:scale-105 transition-transform">
-                                <img
+                                <Image
                                     src="/footy.svg"
                                     alt="Store Logo"
                                     className="h-14 w-14 rounded-full bg-white p-1"
@@ -23,11 +23,7 @@ const Navbar = async () => {
                             </div>
                         </div>
                     </Link>
-
-                    {/* Main Navigation */}
                     <MainNav data={categories} />
-
-                    {/* User and Cart Actions */}
                     <NavBarActions />
                 </div>
             </Container>
