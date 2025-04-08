3ffa7831115b9b36d9d3e98ce6a187357d67002f
10Yash29 <142592494+10Yash29@users.noreply.github.com>
2025-04-08 13:36:20 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -68,8 +68,8 @@ const config = {
           to: { height: "0" },
         },
         "scale-in": {
-          "0%": { transform: "scale(0.95)", opacity: 0 },
-          "100%": { transform: "scale(1)", opacity: 1 },
+          "0%": { transform: "scale(0.95)", opacity: "0" },
+          "100%": { transform: "scale(1)", opacity: "1" },
         },
       },
       animation: {
